d4489b913ae7684abf974f50adc90aec9f4c979f
Zachary Ruffin <zruffin@ncmec.org>
2021-03-17 19:32:21 -0400

    Adding some code for the laser and hero to shoot lasers.

---
+++ Hero.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+
+public class Hero : Node2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	private PackedScene laserScene;
+	
+	private Timer spawnTimer;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		spawnTimer = GetNode<Timer>("SpawnTimer");
+		spawnTimer.Connect("timeout", this, "onSpawnTimeout");
+		laserScene = ResourceLoader.Load<PackedScene>("res://Laser.tscn");
+		SpawnLasers();
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+	 private void SpawnLaser()
+	{
+		var randomX = this.GlobalPosition.x;
+		var randomY = this.GlobalPosition.y;
+			var newPosition = new Vector2(randomX, randomY);
+			var laser = laserScene.Instance() as Laser; 
+			laser.Position = newPosition;
+			
+
+			
+			this.GetParent().AddChild(laser);
+			laser._Ready();
+	}
+
+ public void onSpawnTimeout()
+	{
+		GD.Print("Spawn a laser!");
+		this.SpawnLaser();
+
+		
+	}
+
+	
+
+	public void SpawnLasers()
+	{
+		spawnTimer.WaitTime = 3;
+		spawnTimer.OneShot = false;
+		spawnTimer.Start();
+	}
+
+}
---
+++ Laser.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public class Laser : Node2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	 [Export] public float MaximumSpeed = 500;
+	[Export] public float Acceleration = 2000;
+	// Node2D Laser {get;set;}
+
+	Vector2 CurrentPosition {get;set;}
+	KinematicBody2D LaserBody {get;set;}
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+		LaserBody = GetNode<KinematicBody2D>("KinematicBody2D");
+		// Laser = GetNode<Node2D>("Sprite");
+		GD.Print("I am a laser pew pew!");
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+ public override void _Process(float delta)
+ {
+	 //GD.Print("Tick");
+
+	this.CurrentPosition = this.LaserBody.GlobalPosition;
+
+	 //GD.Print(this.CurrentPosition);
+	 var newYPosition = this.CurrentPosition.y -.0001f;
+
+ 	this.LaserBody.MoveAndSlide(new Vector2(this.CurrentPosition.x, newYPosition));
+	 
+ }
+}
